8e32a4610e3405f0b85d98d4306547c9f50d46d7
cuhadar8 <ibrahimcuhadar8@gmail.com>
2022-10-07 19:18:01 +0300

    StaticDemo

---
+++ StaticDemo/src/DataBaseHelper.java
@@ -0,0 +1,23 @@
+
+public class DataBaseHelper {
+	
+	public static class Crud{// Creat Read Update Delete
+		
+		public static void delete() {
+			
+		}
+		
+		public static void update() {
+			
+		}
+	}
+	
+	public static class Connection{
+		
+		public static void createConnection() {
+			
+		}
+		
+	}
+
+}
